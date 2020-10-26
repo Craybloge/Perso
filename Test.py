d5f94c5585189fae8b925f67a112dcb9272f804a
CyriaqueSoden <CyriaqueSoden@gmail.com>
2020-10-26 09:26:37 +0100

    ajout test.py

---
+++ Test.py
@@ -0,0 +1,19 @@
+import string
+import random
+nom_du_site = "amazon"
+
+
+def cherche_site():
+    "fait des trucs compliqué puis nom_du_site ="
+
+
+def id_generator(size=6, lettre=string.ascii_uppercase + string.digits):
+    return''.join(random.choice(lettre) for _ in range(size))
+
+
+id_generator()
+cherche_site()
+
+save = {nom_du_site: id_generator()}
+print(nom_du_site)
+print(save[nom_du_site])
