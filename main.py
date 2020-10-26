85d90f4cd15579ced097c347332e18a47a3c4efc
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2020-10-26 17:19:55 +0100

    creation fenetre

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import string
 import random
 import tldextract
-import tkinter as tk
+import tkinter
 from class_mdp import Mot_de_Passe as mdp
 url = input("Donnez l'URL  :")
-
 
 
 test = mdp(url)
 
 print(test.nom_site)
 
-fenetre = tk.Tk()
-champ_label = tk.Label(fenetre,  text="wsh wsh les amis")
-champ_label.pack()
+fenetre = tkinter.Tk()
+fenetre.title("Projet Dash Lane")
+
+screen_x = fenetre.winfo_screenwidth()
+screen_y = fenetre.winfo_screenheight()
+
+window_x = 400
+window_Y = 100
+
+
+posX = (screen_x) - (window_x) - 50
+posY = (screen_y) - (window_Y) - 100
+
+
+geo = "{}x{}+{}+{}".format(window_x, window_Y, posX, posY)
+
+fenetre.geometry(geo)
+
+
 fenetre.mainloop()
