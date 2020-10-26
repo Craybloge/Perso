c16f61b3f6f8acc59a0073aa76b28589a8933ff4
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-10-26 16:31:11 +0100

    on rend les commentaires plus propre

--- main.py
+++ main.py
@@ -6,27 +6,34 @@ import csv
 from class_mdp import Mot_de_Passe as mdp
 
 
-url = input("Donnez l'URL  :")                                   #ici on ajoute un mdp 
+# ici on ajoute un mdp
+url = input("Donnez l'URL  :")
 test = mdp(url)
 print(test.nom_site)
 
+
+# ici on initialise les listes
 liste2 = []
-listemdp = []                                                   #ici on initialise les listes
+listemdp = []
 
+
+# la on lit le fichier csv
 with open('stockagemdp.csv', newline="") as csvfile:
-    liste = csv.reader(csvfile, delimiter=' ', quotechar='|')   #la on lit le fichier csv
+    liste = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+    # ici on ajoute les mdp à une liste
     for row in liste:
-        liste2.append(', '.join(row))                           # ici on ajoute les mdp à une liste
+        liste2.append(', '.join(row))
 
-
-    for i in liste2:                                            # ici on sépare les mdp des sites pour pouvoir les repérer
+# ici on sépare les mdp des sites pour pouvoir les repérer
+    for i in liste2:
         i = i.split(',')
         listemdp.append(i)
-print(listemdp)                                             
+print(listemdp)
 
 
-
-choix = input('choisissez le site dont vous voulez le mdp:')    # on demande à l'utilisateur de quel site il veut le mdp et on lui affiche
+# on demande à l'utilisateur de quel site il veut le mdp et on lui affiche
+choix = input('choisissez le site dont vous voulez le mdp:')
 for i in listemdp:
     if choix == i[0]:
         print("voici le mdp:", i[1])
