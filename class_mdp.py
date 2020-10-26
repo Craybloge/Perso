6ea7fdf81c60c6c35d915487dd0ecfb29fd56d55
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-10-26 16:02:44 +0100

    ça marche

--- class_mdp.py
+++ class_mdp.py
@@ -20,7 +20,7 @@ class Mot_de_Passe:
         return''.join(random.choice(lettre) for _ in range(size))
 
     def save(self):  # on les stocke (ici par défaut mais on peut le faire à la demande)
-        with open('stockagemdp.csv', 'w', newline="") as csvfile:
+        with open('stockagemdp.csv', 'a', newline="") as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow([self.nom_site] + [self.mdp])
 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tldextract
 import tkinter as tk
 from class_mdp import Mot_de_Passe as mdp
 url = input("Donnez l'URL  :")
-url = ("twitter.com")
+
 
 
 test = mdp(url)
