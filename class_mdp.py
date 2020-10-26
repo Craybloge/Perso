a42ee4309817171f4f38dc79c225a53a94420fd1
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-10-26 15:22:27 +0100

    ça fonctionne persque

--- class_mdp.py
+++ class_mdp.py
@@ -5,6 +5,7 @@ import tldextract
 
 
 class Mot_de_Passe:
+    
     def __init__(self, nom_site):
         self.nom_site = self.cherche_site(nom_site)
         self.mdp = self.id_generator()
@@ -18,7 +19,7 @@ class Mot_de_Passe:
         return''.join(random.choice(lettre) for _ in range(size))
 
     def save(self):  # on les stocke (ici par défaut mais on peut le faire à la demande)
-        with open('stockagemdp.csv', 'w', newline="") as csvfile:
+        with open('S:\Documents\GitHub\Projet cyriaque\stockagemdp.csv', 'w', newline="") as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow([self.nom_site] + [self.mdp])
 
