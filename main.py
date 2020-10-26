0e52c6a69ffd6d1b0a8b5155e94a8fa5188dc071
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-10-26 15:23:29 +0100

    on supprime les trucs useless

--- main.py
+++ main.py
@@ -9,14 +9,3 @@ test = mdp(url)
 
 print(test.nom_site)
 
-
-# def id_generator(size=6, lettre=string.ascii_uppercase + string.digits):
-#     return''.join(random.choice(lettre) for _ in range(size))
-
-
-# id_generator()
-# cherche_site()
-
-# save = {nom_du_site: id_generator()}
-# print(nom_du_site)
-# print(save[nom_du_site])
